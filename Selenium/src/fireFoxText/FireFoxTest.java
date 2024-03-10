e361b7589e997bd15a2beff3aef5b4a7baa1ab3b
Hari9757 <harichowdary5797@gmail.com>
2024-03-10 23:39:13 +0000

    Created a new Firefox Test

--- Selenium/src/fireFoxText/FireFoxTest.java
+++ Selenium/src/fireFoxText/FireFoxTest.java
@@ -1,9 +1,10 @@
-package fireFoxTest;
+package fireFoxText;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-public class FirefoxTest {
-    public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
+public class FireFoxTest {
+    public static void main(String[] args)
+    {
+    	WebDriver driver = new FirefoxDriver();
 
         driver.get("https://aniwave.to/home");
 
