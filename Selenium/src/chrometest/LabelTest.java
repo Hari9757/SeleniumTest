9926f2381e6de8e687bd57062af739adb4ecf521
Hari9757 <harichowdary5797@gmail.com>
2024-03-16 16:28:37 +0000

    Sample Site Test

--- Selenium/src/chrometest/LabelTest.java
+++ Selenium/src/chrometest/LabelTest.java
@@ -1,5 +1,4 @@
 package chrometest;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class LabelTest {
--- Selenium/src/siteTestSample/Sample1.java
+++ Selenium/src/siteTestSample/Sample1.java
@@ -1,23 +1,33 @@
 package siteTestSample;
-import org.openqa.selenium.By;
+//import org.openqa.selenium.By;
 import java.util.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class Sample1 {
 	public static void main(String[] args) {
     	WebDriver driver = new ChromeDriver();
     	Scanner sc=new Scanner(System.in);
     	System.out.println("Enter the Site URL");
-    	String url= sc.next();
+    	String url= sc.nextLine();
     	//String url="https://naveenprojects.blogspot.com/2023/09/selenium-with-java.html";
     	driver.manage().window().maximize();
     	driver.get(url);
         String page_title = driver.getTitle();
-        WebElement id= driver.findElement(By.id("one"));
-        id.sendKeys("HaC");
-        WebElement name=driver.findElement(By.name("input name"));
-        name.sendKeys("Selenium code");
+        //driver.findElement(By.id("cookieChoiceDismiss")).click();
+        //String ExpectedTitle="Java Projects: Selenium With Java";
+        //if (page_title.equals(ExpectedTitle)) {
+        	//System.out.println("The Test is a Pass");
+        //}
+        //else {
+        	//System.out.println("The Test is a Fail");
+        //}
+        //WebElement idField= driver.findElement(By.id("one"));
+        //idField.sendKeys("HaC");
+       // WebElement nameField=driver.findElement(By.name("input name"));
+        //nameField.sendKeys("Selenium code");
+        //WebElement emailfield= driver.findElement(By.xpath("(//input[@placeholder='email'])[1]"));
+        //emailfield.sendKeys("HaChaparla@gmail.com");
         System.out.println(page_title);
         //driver.quit();
 	}
